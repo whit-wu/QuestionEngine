f68cad233c6181dd746e7adfa81fca20f82dfd13
Whit Wu <whit.wu@gmail.com>
2021-08-31 21:37:26 -0400

    Generate structure of project

---
+++ QuestionEngine.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuestionEngine.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
