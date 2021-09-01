ab6e755ddef880ee44b62d1e7ff8df21ea0cc6a9
Whit Wu <whit.wu@gmail.com>
2021-08-31 21:51:40 -0400

    Add question and answer models

---
+++ QuestionEngine/QuestionEngine.Data/Model/Answer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionEngine.Data.Model
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastModifiedBy { get; set; }
+
+    }
+}
---
+++ QuestionEngine/QuestionEngine.Data/Model/Question.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionEngine.Data.Model
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public List<Answer> AvailableAnswers { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastModifiedBy { get; set; }
+
+    }
+}
