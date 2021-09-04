5f756ea81deab225e58dfe13687077f959ef9000
Whit Wu <whit.wu@gmail.com>
2021-09-03 23:05:00 -0400

    Research the self contained exe error

---
+++ QuestionEngine/Server/Controllers/QuestionEngineController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionEngine.Server.Controllers
+{
+    [Route("controller")]
+    [ApiController]
+    public class QuestionEngineController : ControllerBase
+    {
+        [HttpGet]
+        string Get()
+        {
+            return "Hello there";
+        }
+    }
+}
