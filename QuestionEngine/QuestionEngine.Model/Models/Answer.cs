969a2f85b5615fa4e14b80afbd930f85edd1538f
Whit Wu <whit.wu@gmail.com>
2021-10-09 15:51:01 -0400

    Add a model base

--- QuestionEngine/QuestionEngine.Model/Models/Answer.cs
+++ QuestionEngine/QuestionEngine.Model/Models/Answer.cs
@@ -6,16 +6,11 @@ using System.Threading.Tasks;
 
 namespace QuestionEngine.Model.Models
 {
-    public class Answer
+    public class Answer : ModelBase
     {
         public int Id { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedOn { get; set; }
-        public DateTime LastModifiedOn { get; set; }
-        public string CreatedBy { get; set; }
-        public string LastModifiedBy { get; set; }
         public int QuestionId { get; set; }
-        public Question Question { get; set; }
 
     }
 }
--- QuestionEngine/QuestionEngine.Model/Models/Question.cs
+++ QuestionEngine/QuestionEngine.Model/Models/Question.cs
@@ -7,17 +7,13 @@ using System.Threading.Tasks;
 
 namespace QuestionEngine.Model.Models
 {
-    public class Question
+    public class Question : ModelBase
     {
         public int Id { get; set; }
         public string Description { get; set; }
         public List<Answer> AvailableAnswers { get; } = new List<Answer>();
-        public DateTime CreatedOn { get; set; }
-        public DateTime LastModifiedOn { get; set; }
-        public string CreatedBy { get; set; }
-        public string LastModifiedBy { get; set; }
 
-        public Answer ChosenAnswer { get; set; }
+        public int ChosenAnswerId { get; set; }
 
     }
 }
