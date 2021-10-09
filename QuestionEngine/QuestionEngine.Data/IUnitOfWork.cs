fb6b90356bc7468125188bf06c9ee52b217c3b33
Whit Wu <whit.wu@gmail.com>
2021-10-09 16:45:51 -0400

    Add UoW and Interface

---
+++ QuestionEngine/QuestionEngine.Data/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using QuestionEngine.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionEngine.Data
+{
+    public interface IUnitOfWork
+    {
+        public bool AddQuestion(Question question);
+        public bool UpdateQuestion(Question question);
+        public bool DeleteQuestionById(int id);
+        public Question GetQuestionById(int id);
+
+    }
+}
---
+++ QuestionEngine/QuestionEngine.Data/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using QuestionEngine.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionEngine.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public bool AddQuestion(Question question)
+        {
+
+        }
+        public bool UpdateQuestion(Question question)
+        {
+
+        }
+        public bool DeleteQuestionById(int id)
+        {
+
+        }
+        public Question GetQuestionById(int id)
+        {
+
+        }
+    }
+}
