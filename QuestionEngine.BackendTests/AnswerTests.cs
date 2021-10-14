eedcaefbe72188cc8db31488797f617ef581d7ed
Whit Wu <whit.wu@gmail.com>
2021-10-14 19:30:20 -0400

    Add answers unit test class

---
+++ QuestionEngine.BackendTests/AnswerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using QuestionEngine.Data;
+using QuestionEngine.Model.Models;
+using System;
+using System.Collections.Generic;
+
+namespace QuestionEngine.BackendTests
+{
+    public class AnswerTests
+    {
+        private DbContextOptions options;
+        private QuestionEngineContext _context;
+        private string userId = "SYSTEM";
+        private IUnitOfWork uow;
+
+        [SetUp]
+        public void SetupContext()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            options = new DbContextOptionsBuilder<QuestionEngineContext>().UseSqlite(connection).Options;
+
+
+            // allows tables to be created first
+            using (var migContext = new QuestionEngineContext(options))
+            {
+                migContext.Database.EnsureCreated();
+            }
+
+
+            // the actual context
+            _context = new QuestionEngineContext(options);
+
+            uow = new UnitOfWork(_context);
+        }
+    }
+}
