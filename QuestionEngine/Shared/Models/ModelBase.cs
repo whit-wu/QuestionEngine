71eee72b337d29f1d9219ea62de7e8bcf364eede
Whit Wu <whit.wu@gmail.com>
2021-11-28 20:57:21 -0500

    Fix naming convention for rule

--- QuestionEngine/Shared/Models/ModelBase.cs
+++ QuestionEngine/Shared/Models/ModelBase.cs
@@ -1,9 +1,6 @@
 ﻿using QuestionEngine.Shared.Rules;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuestionEngine.Shared.Models
 {
